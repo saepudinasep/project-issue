90a2aa8765337e9c648c427eff2d8648e6c79e30
saepudinasep <saepudinasep2001@gmail.com>
2024-10-20 14:52:22 +0700

    [ADD] Menambahkan migration role dan merubah email menjadi nik di user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'nik',
         'name',
-        'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -41,8 +42,13 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            // 'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
